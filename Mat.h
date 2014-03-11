5b9a133218c2f9e053eac65bb2202efad3b71428
R-Lai <sc0916682506@gmail.com>
2014-03-12 02:53:19 +0900

    add  Mat.cpp  Mat.h

---
+++ Mat.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Vec.h"
+using namespace std;
+
+class Mat
+{
+public:
+	Mat::Mat();
+	Mat::~Mat();
+	Vec *vec;
+	int i,j,k;
+	vector<vector<vector<double>>> M; //儲存所有陣列
+	void push(vector<vector<double>> x);
+	
+	vector<vector<double>> pop(double a,char x);  //取出陣列
+	string print_out(vector<vector<double>> a);  //印出陣列
+	vector<vector<double>> add(double a,char x,double b,char y);  //加法
+	vector<vector<double>> add(double a,char x,vector<vector<double>> ve);
+	vector<vector<double>> dec(double a,char x,vector<vector<double>> ve);  //減法
+
+
+
+};
